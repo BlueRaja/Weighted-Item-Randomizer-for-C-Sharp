b301cea5ec75fab02a26d312426e2e99a15145cf
BlueRaja <BlueRaja.admin@gmail.com>
2013-07-12 00:42:19 -0500

    Initial commit from other project

---
+++ Weighted Randomizer/FastRemovalWeightedRandomizer.cs
@@ -0,0 +1,553 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Weighted_Randomizer
+{
+    /// <summary>
+    /// A class which constructs a tree of weighted objects.  Once constructed, the caller can choose one at random random
+    /// (by weight, so higher- weighted items are more likely to be chosen), with or without replacement; add new objects; 
+    /// remove objects; update the weight of objects; and check for objects within the list.  All of these operations are O(log n).
+    /// </summary>
+    /// <typeparam name="TKey">The type of the objects to choose at random</typeparam>
+    public class FastRemovalWeightedRandomizer<TKey> : IWeightedRandomizer<TKey>
+        where TKey : IComparable<TKey>
+    {
+        private readonly Node sentinel;
+        private readonly ThreadSafeRandom random;
+        private Node root;
+        private Node deleted;
+
+        public FastRemovalWeightedRandomizer()
+        {
+            root = sentinel = new Node();
+            deleted = null;
+            random = new ThreadSafeRandom();
+        }
+
+        //public FastRemovalWeightedRandomizer(int seed)
+        //{
+        //    root = sentinel = new Node();
+        //    deleted = null;
+        //    random = new ThreadSafeRandom(seed);
+        //}
+
+        #region Node class
+        private class Node
+        {
+            // AA-tree data
+            internal int level;
+            internal Node left;
+            internal Node right;
+
+            //Weighted Randomizer data
+            internal TKey key;
+            internal int weight;
+            internal long subtreeWeight;
+
+            // constuctor for the sentinel node
+            internal Node()
+            {
+                this.level = 0;
+                this.left = this;
+                this.right = this;
+                this.weight = 0;
+                this.subtreeWeight = 0;
+            }
+
+            // constuctor for regular nodes (that all start life as leaf nodes)
+            internal Node(TKey key, int weight, Node sentinel)
+            {
+                this.level = 1;
+                this.left = sentinel;
+                this.right = sentinel;
+                this.key = key;
+                this.weight = weight;
+                this.subtreeWeight = weight;
+            }
+        }
+        #endregion
+
+        #region AA-Tree code
+        //This weighted randomizer can be used with any self-balancing tree.  I chose an AA-tree written by Aleksey Demakov
+        //(with his permission of course), found here:  http://demakov.com/snippets/aatree.html
+        //I chose this one because it was easy to understand and well-written.
+
+        private void RotateRight(ref Node node)
+        {
+            if (node.level == node.left.level)
+            {   
+                //Update the subtreeWeights before moving the nodes
+                long oldSubtreeWeight = node.subtreeWeight;
+                if (node != sentinel)
+                    node.subtreeWeight = oldSubtreeWeight - node.left.subtreeWeight + node.left.right.subtreeWeight;
+                if (node.left != sentinel)
+                    node.left.subtreeWeight = oldSubtreeWeight;
+
+                // rotate right
+                Node left = node.left;
+                node.left = left.right;
+                left.right = node;
+                node = left;
+            }
+        }
+
+        private void RotateLeft(ref Node node)
+        {
+            if (node.right.right.level == node.level)
+            {
+                //Update the subtreeWeights before moving the nodes
+                long oldSubtreeWeight = node.subtreeWeight;
+                if (node != sentinel)
+                    node.subtreeWeight = oldSubtreeWeight - node.right.subtreeWeight + node.right.left.subtreeWeight;
+                if (node.right != sentinel)
+                    node.right.subtreeWeight = oldSubtreeWeight;
+
+                // rotate left
+                Node right = node.right;
+                node.right = right.left;
+                right.left = node;
+                node = right;
+                node.level++;
+            }
+        }
+
+        private void InsertNode(ref Node node, TKey key, int weight)
+        {
+            if (node == sentinel)
+            {
+                node = new Node(key, weight, sentinel);
+                UpdateSubtreeWeightsForInsertion(node);
+                Count++;
+                return;
+            }
+
+            int compare = key.CompareTo(node.key);
+            if (compare < 0)
+            {
+                InsertNode(ref node.left, key, weight);
+            }
+            else if (compare > 0)
+            {
+                InsertNode(ref node.right, key, weight);
+            }
+            else
+            {
+                throw new ArgumentException("Key already exists in FastRemovalWeightedRandomizer: " + key.ToString());
+            }
+
+            RotateRight(ref node);
+            RotateLeft(ref node);
+        }
+
+        private bool DeleteNode(ref Node node, TKey key)
+        {
+            if (node == sentinel)
+            {
+                return (deleted != null);
+            }
+
+            int compare = key.CompareTo(node.key);
+            if (compare < 0)
+            {
+                if (!DeleteNode(ref node.left, key))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (compare == 0)
+                {
+                    deleted = node;
+                }
+                if (!DeleteNode(ref node.right, key))
+                {
+                    return false;
+                }
+            }
+
+            if (deleted != null)
+            {
+                UpdateSubtreeWeightsForDeletion(deleted, node);
+                deleted.key = node.key;
+                deleted.weight = node.weight;
+                deleted = null;
+                node = node.right;
+                Count--;
+            }
+            else if (node.left.level < node.level - 1
+                     || node.right.level < node.level - 1)
+            {
+                --node.level;
+                if (node.right.level > node.level)
+                {
+                    node.right.level = node.level;
+                }
+                RotateRight(ref node);
+                RotateRight(ref node.right);
+                RotateRight(ref node.right.right);
+                RotateLeft(ref node);
+                RotateLeft(ref node.right);
+            }
+
+            return true;
+        }
+
+        private Node FindNode(Node node, TKey key)
+        {
+            while (node != sentinel)
+            {
+                int compare = key.CompareTo(node.key);
+                if (compare < 0)
+                {
+                    node = node.left;
+                }
+                else if (compare > 0)
+                {
+                    node = node.right;
+                }
+                else
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+        #endregion
+
+        #region Updating weights
+        //When inserting, all the subtree's parent subtrees go up by insertedNode.weight
+        //Should be called AFTER the insertion
+        private void UpdateSubtreeWeightsForInsertion(Node insertedNode)
+        {
+            //Search down the tree for the inserted node, updating the weights as we go
+            Node currentNode = root;
+            while (currentNode != insertedNode)
+            {
+                currentNode.subtreeWeight += insertedNode.weight;
+
+                int compare = insertedNode.key.CompareTo(currentNode.key);
+                currentNode = (compare < 0 ? currentNode.left : currentNode.right);
+            }
+        }
+
+        //When deleting a node, we swap it with its leftmost-descendent in the RIGHT subtree, then remove it.
+        //Thus, the weight of deleteMe's subtree (and all parent subtrees) goes down by deleteMe.weight,
+        //while all the children subtrees go that contain leftmostRightDescendent go down by leftmostRightDescendent.weight
+        //Should be called BEFORE either the swap or deletion
+        private void UpdateSubtreeWeightsForDeletion(Node deletedNode, Node leftmostRightDescendent)
+        {
+            //Search down the tree for the deletedNode, updating the weights as we go
+            Node currentNode = root;
+            while (currentNode != deletedNode)
+            {
+                currentNode.subtreeWeight -= deletedNode.weight;
+
+                int compare = deletedNode.key.CompareTo(currentNode.key);
+                currentNode = (compare < 0 ? currentNode.left : currentNode.right);
+            } 
+
+            //Right now, currentNode == deletedNode
+            currentNode.subtreeWeight -= deletedNode.weight;
+
+            //Deleted node gets replaced by leftmostRightDescendent, so we need to continue searching the subtree for it instead,
+            //again updating the nodes' weights as we go
+            while (currentNode != leftmostRightDescendent && currentNode != sentinel)
+            {
+                int compare = leftmostRightDescendent.key.CompareTo(currentNode.key);
+                currentNode = (compare < 0 ? currentNode.left : currentNode.right);
+
+                currentNode.subtreeWeight -= leftmostRightDescendent.weight;
+            }
+
+            //At this point, the subtreeWeights have been correctly subtracted from, but the
+            //actual node values haven't been swapped yet
+        }
+        #endregion
+
+        #region ICollection<T> stuff
+        /// <summary>
+        /// Returns the number of items currently in the list
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Remove all items from the list
+        /// </summary>
+        public void Clear()
+        {
+            root = sentinel;
+        }
+
+        /// <summary>
+        /// Returns false.  Necessary for the ICollection&lt;T&gt; interface.
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Copies the keys to an array, in order
+        /// </summary>
+        public void CopyTo(TKey[] array, int startingIndex)
+        {
+            int currentIndex = startingIndex;
+            foreach (TKey key in this)
+            {
+                array[currentIndex] = key;
+                currentIndex++;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the given item has been added to the list; false otherwise
+        /// </summary>
+        public bool Contains(TKey key)
+        {
+            Node node = FindNode(root, key);
+            return (node != null && node != sentinel);
+        }
+
+        /// <summary>
+        /// Adds the given item with a default weight of 1
+        /// </summary>
+        public void Add(TKey key)
+        {
+            InsertNode(ref root, key, 1);
+        }
+
+        /// <summary>
+        /// Adds the given item with the given weight
+        /// </summary>
+        public void Add(TKey key, int weight)
+        {
+            InsertNode(ref root, key, weight);
+        }
+
+        /// <summary>
+        /// Remoevs the given item from the list.
+        /// </summary>
+        /// <returns>Returns true if the item was successfully deleted, or false if it was not found</returns>
+        public bool Remove(TKey key)
+        {
+            deleted = null;
+            return DeleteNode(ref root, key);
+        }
+        #endregion
+
+        #region IEnumerable<T> stuff
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<TKey> GetEnumerator()
+        {
+            return InorderTraversal(root);
+        }
+
+        private IEnumerator<TKey> InorderTraversal(Node node)
+        {
+            //The obvious way of doing this - calling itself recursively - ends up creating an enormous number
+            // of iterators (as many as there are items in the tree).  So, we have to emulate recursion manually >_<
+            Stack<Node> stack = new Stack<Node>();
+            while (stack.Count != 0 || node != sentinel)
+            {
+                if (node != sentinel)
+                {
+                    stack.Push(node);
+                    node = node.left;
+                }
+                else
+                {
+                    node = stack.Pop();
+                    yield return node.key;
+                    node = node.right;
+                }
+            }
+        }
+        #endregion
+
+        #region Other public methods
+        /// <summary>
+        /// The total weight of all the items added so far
+        /// </summary>
+        public long TotalWeight
+        {
+            get
+            {
+                return root.subtreeWeight;
+            }
+        }
+
+        /// <summary>
+        /// Returns an item chosen randomly by weight (higher weights are more likely),
+        /// and replaces it so that it can be chosen again
+        /// </summary>
+        public TKey NextWithReplacement()
+        {
+            if (Count <= 0)
+                throw new InvalidOperationException("There are no items in the FastRemovalWeightedRandomizer");
+
+            Node currentNode = root;
+            long randomNumber = random.NextLong(0, TotalWeight) + 1;  //[1, TotalWeight] inclusive
+
+            while (true)
+            {
+                if (currentNode.left.subtreeWeight >= randomNumber)
+                {
+                    currentNode = currentNode.left;
+                }
+                else
+                {
+                    randomNumber -= currentNode.left.subtreeWeight;
+                    if (currentNode.right.subtreeWeight >= randomNumber)
+                    {
+                        currentNode = currentNode.right;
+                    }
+                    else
+                    {
+                        return currentNode.key;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an item chosen randomly by weight (higher weights are more likely),
+        /// and removes it so it cannot be chosen again
+        /// </summary>
+        public TKey NextWithRemoval()
+        {
+            if (Count <= 0)
+                throw new InvalidOperationException("There are no items in the FastRemovalWeightedRandomizer");
+
+            TKey randomKey = NextWithReplacement();
+            Remove(randomKey);
+            return randomKey;
+        }
+
+        /// <summary>
+        /// Shortcut syntax to add, remove, and update an item
+        /// </summary>
+        public int this[TKey key]
+        {
+            get
+            {
+                return GetWeight(key);
+            }
+            set
+            {
+                SetWeight(key, value);
+            }
+        }
+
+        /// <summary>
+        /// Returns the weight of the given item.  Throws an exception if the item is not added
+        /// (use .Contains to check first if unsure)
+        /// </summary>
+        public int GetWeight(TKey key)
+        {
+            Node node = FindNode(root, key);
+            if (node == null)
+                throw new ArgumentException("Key not found in FastRemovalWeightedRandomizer: " + key);
+            return node.weight;
+        }
+
+        /// <summary>
+        /// Updates the weight of the given item, or adds it if it has not already been added.
+        /// If weight &lt;= 0, the item is removed.
+        /// </summary>
+        public void SetWeight(TKey key, int weight)
+        {
+            if (weight <= 0)
+            {
+                Remove(key);
+            }
+            else
+            {
+                Node node = FindNode(root, key);
+                if (node == null)
+                {
+                    Add(key, weight);
+                }
+                else
+                {
+                    int weightDelta = weight - node.weight;
+
+                    //This is a hack.  The point is to update this node's and all it's ancestors' subtreeWeights.
+                    //We already have a method that will do that; however, it uses the value of node.weight, rather
+                    //than a parameter.
+                    node.weight = weightDelta;
+                    UpdateSubtreeWeightsForInsertion(node);
+                    
+                    //Finally, set the node.weight to what it should be
+                    node.weight = weight;
+                    node.subtreeWeight += weightDelta;
+
+#if DEBUG //TODO: DELETE
+                    DebugCheckTree();
+#endif
+                }
+            }
+        }
+        #endregion
+
+        #region Debugging code
+        /// <summary>
+        /// Returns the height of the tree (very slow)
+        /// </summary>
+        private int Height
+        {
+            get
+            {
+                return GetNumLayers(root);
+            }
+        }
+
+        private int GetNumLayers(Node node)
+        {
+            if (node == null || node == sentinel)
+                return 0;
+            return Math.Max(GetNumLayers(node.left), GetNumLayers(node.right)) + 1;
+        }
+
+        /// <summary>
+        /// Quick hack to write quick tests
+        /// </summary>
+        private void Assert(bool condition)
+        {
+            if (!condition)
+                throw new ArgumentException("Test case failed");
+        }
+
+        /// <summary>
+        /// Make sure the entire tree is valid (correct subtreeWeights, valid BST, that sort of thing)
+        /// </summary>
+        private void DebugCheckTree()
+        {
+            DebugCheckNode(root);
+            Assert(Count == 0 || Height <= 2 * Math.Ceiling(Math.Log(Count, 2) + 1));
+        }
+
+        private void DebugCheckNode(Node node)
+        {
+            if (node == null || node == sentinel)
+                return;
+
+            Assert(node.left == null || node.left == sentinel || node.left.key.CompareTo(node.key) < 0);
+            Assert(node.right == null || node.right == sentinel || node.right.key.CompareTo(node.key) > 0);
+            Assert(node.left.subtreeWeight + node.right.subtreeWeight + node.weight == node.subtreeWeight);
+
+            DebugCheckNode(node.left);
+            DebugCheckNode(node.right);
+        }
+        #endregion
+    }
+}
---
+++ Weighted Randomizer/ThreadSafeRandom.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Weighted_Randomizer
+{
+    public class ThreadSafeRandom
+    {
+        private static readonly Random _global = new Random();
+
+        [ThreadStatic]
+        private static Random _local;
+
+        public ThreadSafeRandom()
+        {
+            if (_local == null)
+            {
+                //Instantiating multiple Random() instances in a row very quickly will result in
+                //all of them returning the same numbers.  This is a workaround for that problem.
+                int seed;
+                lock (_global)
+                {
+                    seed = _global.Next();
+                }
+                _local = new Random(seed);
+            }
+        }
+
+        public int Next()
+        {
+            return _local.Next();
+        }
+
+        public int Next(int maxValue)
+        {
+            return _local.Next(maxValue);
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            return _local.Next(minValue, maxValue);
+        }
+
+        public double NextDouble()
+        {
+            return _local.NextDouble();
+        }
+
+        public long NextLong()
+        {
+            return _local.NextLong();
+        }
+
+        public long NextLong(long max)
+        {
+            return _local.NextLong(max);
+        }
+
+        public long NextLong(long min, long max)
+        {
+            return _local.NextLong(min, max);
+        }
+    }
+}
