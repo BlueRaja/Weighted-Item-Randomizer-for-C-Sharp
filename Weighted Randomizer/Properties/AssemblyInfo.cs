f17ae5177ddeb14533d47b6127b2396a32c82ca3
BlueRaja <BlueRaja.admin@gmail.com>
2016-01-23 01:41:35 -0600

    Added NuGet .nuspec file

--- Weighted Randomizer/Properties/AssemblyInfo.cs
+++ Weighted Randomizer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Weighted Randomizer")]
-[assembly: AssemblyDescription("A collection which allows fast random selection of items")]
+[assembly: AssemblyDescription("A data structure which allows fast random selection of items")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Weighted Randomizer")]
