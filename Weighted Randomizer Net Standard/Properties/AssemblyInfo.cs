b7fd097e06ee92a5467f14ecb8a6943334c5a803
BlueRaja <BlueRaja.admin@gmail.com>
2020-08-09 15:42:05 -0700

    Fixed incorrect DLL name

--- Weighted Randomizer Net Standard/Properties/AssemblyInfo.cs
+++ Weighted Randomizer Net Standard/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Weighted Randomizer")]
-[assembly: AssemblyCopyright("Copyright © BlueRaja 2017")]
+[assembly: AssemblyCopyright("Copyright © BlueRaja 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
